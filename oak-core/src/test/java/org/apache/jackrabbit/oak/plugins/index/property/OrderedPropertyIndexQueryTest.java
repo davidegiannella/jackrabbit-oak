4b7b352258c4d712ae21c9d74ad2d466e62729bb
Davide Giannella <giannella.davide@gmail.com>
2014-02-20 15:04:35 +0100

    OAK-1263 applied the formatter

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/property/OrderedPropertyIndexQueryTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/property/OrderedPropertyIndexQueryTest.java
@@ -49,234 +49,244 @@ import org.apache.jackrabbit.oak.util.NodeUtil;
 import org.junit.Test;
 
 public class OrderedPropertyIndexQueryTest extends AbstractQueryTest {
-   /**
-    * the property used by the index
-    */
-   public final static String ORDERED_PROPERTY = "foo";
+    /**
+     * the property used by the index
+     */
+    public final static String ORDERED_PROPERTY = "foo";
 
-   /**
-    * number of nodes to create for testing.
-    * 
-    * It has been found during development that in some cases the order of the nodes
-    * creation within the persistence where the actual expected order.
-    * 
-    * The higher the value the lower the chance for this to happen.
-    */
-   private final static int NUMBER_OF_NODES = 50; 
+    /**
+     * number of nodes to create for testing.
+     * 
+     * It has been found during development that in some cases the order of the
+     * nodes creation within the persistence where the actual expected order.
+     * 
+     * The higher the value the lower the chance for this to happen.
+     */
+    private final static int NUMBER_OF_NODES = 50;
 
-   /**
-    * convenience orderable object that represet a tuple of values and paths
-    * 
-    * where the values are the indexed keys from the index and the paths are the path 
-    * which hold the key
-    */
-   private class ValuePathTuple implements Comparable<ValuePathTuple>{
-      private String value;
-      private String path;
+    /**
+     * convenience orderable object that represet a tuple of values and paths
+     * 
+     * where the values are the indexed keys from the index and the paths are
+     * the path which hold the key
+     */
+    private class ValuePathTuple implements Comparable<ValuePathTuple> {
+        private String value;
+        private String path;
 
-      ValuePathTuple(String value, String path) {
-         this.value=value;
-         this.path=path;
-      }
+        ValuePathTuple(String value, String path) {
+            this.value = value;
+            this.path = path;
+        }
 
-      @Override
-      public int hashCode() {
-         final int prime = 31;
-         int result = 1;
-         result = prime * result + getOuterType().hashCode();
-         result = prime * result + ((path == null) ? 0 : path.hashCode());
-         result = prime * result + ((value == null) ? 0 : value.hashCode());
-         return result;
-      }
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + getOuterType().hashCode();
+            result = prime * result + ((path == null) ? 0 : path.hashCode());
+            result = prime * result + ((value == null) ? 0 : value.hashCode());
+            return result;
+        }
 
-      @Override
-      public boolean equals(Object obj) {
-         if (this == obj)
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            ValuePathTuple other = (ValuePathTuple) obj;
+            if (!getOuterType().equals(other.getOuterType()))
+                return false;
+            if (path == null) {
+                if (other.path != null)
+                    return false;
+            } else if (!path.equals(other.path))
+                return false;
+            if (value == null) {
+                if (other.value != null)
+                    return false;
+            } else if (!value.equals(other.value))
+                return false;
             return true;
-         if (obj == null)
-            return false;
-         if (getClass() != obj.getClass())
-            return false;
-         ValuePathTuple other = (ValuePathTuple) obj;
-         if (!getOuterType().equals(other.getOuterType()))
-            return false;
-         if (path == null) {
-            if (other.path != null)
-               return false;
-         } else if (!path.equals(other.path))
-            return false;
-         if (value == null) {
-            if (other.value != null)
-               return false;
-         } else if (!value.equals(other.value))
-            return false;
-         return true;
-      }
+        }
 
-      @Override
-      public int compareTo(ValuePathTuple o) {
-         if(this.equals(o)) return 0;
+        @Override
+        public int compareTo(ValuePathTuple o) {
+            if (this.equals(o))
+                return 0;
 
-         if(this.value.compareTo(o.value)<0) return -1;
-         if(this.value.compareTo(o.value)>0) return 1;
+            if (this.value.compareTo(o.value) < 0)
+                return -1;
+            if (this.value.compareTo(o.value) > 0)
+                return 1;
 
-         if(this.path.compareTo(o.path)<0) return -1;
-         if(this.path.compareTo(o.path)>0) return 1;
+            if (this.path.compareTo(o.path) < 0)
+                return -1;
+            if (this.path.compareTo(o.path) > 0)
+                return 1;
 
-         return 0;
-      }
+            return 0;
+        }
 
-      private OrderedPropertyIndexQueryTest getOuterType() {
-         return OrderedPropertyIndexQueryTest.this;
-      }
+        private OrderedPropertyIndexQueryTest getOuterType() {
+            return OrderedPropertyIndexQueryTest.this;
+        }
 
-   }
+    }
 
-   /**
-    * testing for asserting the right comparison behaviour of the custom class
-    */
-   @Test public void valuePathTupleComparison(){
-      try{
-         new ValuePathTuple("value", "path").compareTo(null);
-         fail("It should have raisen a NPE");
-      }catch(NullPointerException e){
-         //so far so good
-      }
-      assertEquals(0,(new ValuePathTuple("value", "path")).compareTo(new ValuePathTuple("value", "path")));
-      assertEquals(-1,(new ValuePathTuple("value", "path")).compareTo(new ValuePathTuple("value1", "path")));
-      assertEquals(-1,(new ValuePathTuple("value1", "path")).compareTo(new ValuePathTuple("value1", "path1")));
-      assertEquals(1,(new ValuePathTuple("value1", "path")).compareTo(new ValuePathTuple("value", "path")));
-      assertEquals(1,(new ValuePathTuple("value1", "path1")).compareTo(new ValuePathTuple("value1", "path")));
+    /**
+     * testing for asserting the right comparison behaviour of the custom class
+     */
+    @Test
+    public void valuePathTupleComparison() {
+        try {
+            new ValuePathTuple("value", "path").compareTo(null);
+            fail("It should have raisen a NPE");
+        } catch (NullPointerException e) {
+            // so far so good
+        }
+        assertEquals(0, (new ValuePathTuple("value", "path")).compareTo(new ValuePathTuple("value", "path")));
+        assertEquals(-1, (new ValuePathTuple("value", "path")).compareTo(new ValuePathTuple("value1", "path")));
+        assertEquals(-1, (new ValuePathTuple("value1", "path")).compareTo(new ValuePathTuple("value1", "path1")));
+        assertEquals(1, (new ValuePathTuple("value1", "path")).compareTo(new ValuePathTuple("value", "path")));
+        assertEquals(1, (new ValuePathTuple("value1", "path1")).compareTo(new ValuePathTuple("value1", "path")));
 
-      assertEquals(-1,(new ValuePathTuple("value000", "/test/n1")).compareTo(new ValuePathTuple("value001", "/test/n0")));
-      assertEquals(1,(new ValuePathTuple("value001", "/test/n0")).compareTo(new ValuePathTuple("value000", "/test/n1")));
-   }
+        assertEquals(-1, (new ValuePathTuple("value000", "/test/n1")).compareTo(new ValuePathTuple("value001",
+                        "/test/n0")));
+        assertEquals(1, (new ValuePathTuple("value001", "/test/n0")).compareTo(new ValuePathTuple("value000",
+                        "/test/n1")));
+    }
 
-   @Override
-   protected ContentRepository createRepository() {
-      return new Oak().with(new InitialContent())
-            .with(new OpenSecurityProvider())
-            //            .with(new PropertyIndexProvider())
-            //            .with(new PropertyIndexEditorProvider())
-            .with(new OrderedPropertyIndexProvider())
-            .with(new OrderedPropertyIndexEditorProvider())
-            .createContentRepository();
-   }
+    @Override
+    protected ContentRepository createRepository() {
+        return new Oak().with(new InitialContent())
+                        .with(new OpenSecurityProvider())
+                        // .with(new PropertyIndexProvider())
+                        // .with(new PropertyIndexEditorProvider())
+                        .with(new OrderedPropertyIndexProvider()).with(new OrderedPropertyIndexEditorProvider())
+                        .createContentRepository();
+    }
 
-   /**
-    * create a child node for the provided father
-    * 
-    * @param father
-    * @param name the name of the node to create
-    * @param propName the name of the property to assign
-    * @param propValue the value of the property to assign
-    * @return
-    */
-   private Tree child(Tree father, String name, String propName, String propValue){
-      Tree child = father.addChild(name);
-      child.setProperty(JCR_PRIMARYTYPE, NT_UNSTRUCTURED, Type.NAME);
-      child.setProperty(propName,propValue,Type.STRING);
-      return child;
-   }
+    /**
+     * create a child node for the provided father
+     * 
+     * @param father
+     * @param name
+     *            the name of the node to create
+     * @param propName
+     *            the name of the property to assign
+     * @param propValue
+     *            the value of the property to assign
+     * @return
+     */
+    private Tree child(Tree father, String name, String propName, String propValue) {
+        Tree child = father.addChild(name);
+        child.setProperty(JCR_PRIMARYTYPE, NT_UNSTRUCTURED, Type.NAME);
+        child.setProperty(propName, propValue, Type.STRING);
+        return child;
+    }
 
-   /**
-    * generate a list of values to be used as ordered set. Will return something like
-    * {@code value000, value001, value002, ...}
-    *
-    * 
-    * @param amount
-    * @return
-    */
-   private List<String> generateOrderedValues(int amount){
-      if(amount>1000) throw new RuntimeException("amount cannot be greater than 100");
+    /**
+     * generate a list of values to be used as ordered set. Will return
+     * something like {@code value000, value001, value002, ...}
+     * 
+     * 
+     * @param amount
+     * @return
+     */
+    private List<String> generateOrderedValues(int amount) {
+        if (amount > 1000)
+            throw new RuntimeException("amount cannot be greater than 100");
 
-      List<String> values = new ArrayList<String>(amount);
-      NumberFormat nf = new DecimalFormat("000");
-      for(int i=0;i<amount;i++) values.add( String.format("value%s",String.valueOf(nf.format(i))) );
+        List<String> values = new ArrayList<String>(amount);
+        NumberFormat nf = new DecimalFormat("000");
+        for (int i = 0; i < amount; i++)
+            values.add(String.format("value%s", String.valueOf(nf.format(i))));
 
-      return values;
-   }
+        return values;
+    }
 
-   /**
-    * convenience method that adds a bunch of nodes in random order and return the order in which 
-    * they should be presented by the OrderedIndex
-    * 
-    * @param values the values of the property that will be indexed
-    * @param father the father under which add the ndoes
-    * @return
-    */
-   private List<ValuePathTuple> addChildNodes(final List<String> values, final Tree father){
-      List<ValuePathTuple> nodes = new ArrayList<ValuePathTuple>();
-      Random rnd = new Random();
-      int counter = 0;
-      while(values.size()>0){
-         String v = values.remove(rnd.nextInt(values.size()));
-         Tree t = child(father, String.format("n%s",counter++), ORDERED_PROPERTY, v);
-         nodes.add(new ValuePathTuple(v, t.getPath()));
-      }
+    /**
+     * convenience method that adds a bunch of nodes in random order and return
+     * the order in which they should be presented by the OrderedIndex
+     * 
+     * @param values
+     *            the values of the property that will be indexed
+     * @param father
+     *            the father under which add the ndoes
+     * @return
+     */
+    private List<ValuePathTuple> addChildNodes(final List<String> values, final Tree father) {
+        List<ValuePathTuple> nodes = new ArrayList<ValuePathTuple>();
+        Random rnd = new Random();
+        int counter = 0;
+        while (values.size() > 0) {
+            String v = values.remove(rnd.nextInt(values.size()));
+            Tree t = child(father, String.format("n%s", counter++), ORDERED_PROPERTY, v);
+            nodes.add(new ValuePathTuple(v, t.getPath()));
+        }
 
-      Collections.sort(nodes);
-      return nodes;
-   }
+        Collections.sort(nodes);
+        return nodes;
+    }
 
+    @Override
+    protected void createTestIndexNode() throws Exception {
+        Tree index = root.getTree("/");
+        IndexUtils.createIndexDefinition(new NodeUtil(index.getChild(IndexConstants.INDEX_DEFINITIONS_NAME)),
+                        TEST_INDEX_NAME, false, new String[] { ORDERED_PROPERTY }, null, OrderedIndex.TYPE);
+        root.commit();
+    }
 
+    /**
+     * Query the index for retrieving all the entries
+     * 
+     * @throws CommitFailedException
+     * @throws ParseException
+     * @throws RepositoryException
+     */
+    @Test
+    public void queryAllEntries() throws CommitFailedException, ParseException, RepositoryException {
+        setTravesalEnabled(false);
 
-   @Override
-   protected void createTestIndexNode() throws Exception {
-      Tree index = root.getTree("/");
-      IndexUtils.createIndexDefinition(
-            new NodeUtil(index.getChild(IndexConstants.INDEX_DEFINITIONS_NAME)),
-            TEST_INDEX_NAME, 
-            false, 
-            new String[] { ORDERED_PROPERTY },
-            null,
-            OrderedIndex.TYPE
-            );
-      root.commit();
-   }
+        // index automatically created by the framework: {@code
+        // createTestIndexNode()}
 
-   /**
-    * Query the index for retrieving all the entries
-    * 
-    * @throws CommitFailedException
-    * @throws ParseException
-    * @throws RepositoryException
-    */
-   @Test public void queryAllEntries() throws CommitFailedException, ParseException, RepositoryException{
-      setTravesalEnabled(false);
+        Tree rTree = root.getTree("/");
+        Tree test = rTree.addChild("test");
+        List<ValuePathTuple> nodes = addChildNodes(generateOrderedValues(NUMBER_OF_NODES), test);
+        root.commit();
 
-      //index automatically created by the framework: {@code createTestIndexNode()}
-      
-      Tree rTree = root.getTree("/");
-      Tree test = rTree.addChild("test");
-      List<ValuePathTuple> nodes = addChildNodes(generateOrderedValues(NUMBER_OF_NODES),test);
-      root.commit();
+        // querying
+        Iterator<? extends ResultRow> results;
+        results = executeQuery(String.format("SELECT * from [%s] WHERE foo IS NOT NULL", NT_UNSTRUCTURED), SQL2, null)
+                        .getRows().iterator();
+        assertRightOrder(nodes, results);
 
-      //querying
-      Iterator<? extends ResultRow> results;
-      results = executeQuery(
-            String.format("SELECT * from [%s] WHERE foo IS NOT NULL", NT_UNSTRUCTURED), 
-            SQL2, 
-            null
-            ).getRows().iterator();
-      assertRightOrder(nodes, results);
+        setTravesalEnabled(true);
+    }
 
-      setTravesalEnabled(true);
-   }
-
-   /**
-    * assert the right order of the returned resultset
-    * 
-    * @param orderedSequence the right order in which the resultset should be returned
-    * @param resultset the resultes
-    */
-   private void assertRightOrder(@Nonnull final List<ValuePathTuple> orderedSequence, @Nonnull final Iterator<? extends ResultRow> resultset){
-      assertTrue("No results returned", resultset.hasNext());
-      int counter = 0;
-      while(resultset.hasNext() && counter < orderedSequence.size()){
-         ResultRow row = resultset.next();
-         assertEquals(String.format("Wrong path at the element '%d'",counter), orderedSequence.get(counter).path,row.getPath());
-         counter++;
-      }
-   }
+    /**
+     * assert the right order of the returned resultset
+     * 
+     * @param orderedSequence
+     *            the right order in which the resultset should be returned
+     * @param resultset
+     *            the resultes
+     */
+    private void assertRightOrder(@Nonnull
+    final List<ValuePathTuple> orderedSequence, @Nonnull
+    final Iterator<? extends ResultRow> resultset) {
+        assertTrue("No results returned", resultset.hasNext());
+        int counter = 0;
+        while (resultset.hasNext() && counter < orderedSequence.size()) {
+            ResultRow row = resultset.next();
+            assertEquals(String.format("Wrong path at the element '%d'", counter), orderedSequence.get(counter).path,
+                            row.getPath());
+            counter++;
+        }
+    }
 }
