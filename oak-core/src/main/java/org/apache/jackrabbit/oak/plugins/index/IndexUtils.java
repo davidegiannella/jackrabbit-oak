84f65eacc9e41b0c6b060a849ecfc952cf6f0ee6
Davide Giannella <giannella.davide@gmail.com>
2014-03-12 10:55:23 +0000

    OAK-1263 method for defining an index starting from NodeBuilder

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/IndexUtils.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/IndexUtils.java
@@ -208,23 +208,24 @@ public class IndexUtils {
     public static void createIndexDefinition(@Nonnull NodeBuilder indexNode, 
                                              @Nonnull String indexDefName, 
                                              boolean unique, 
-                                             @Nonnull String[] propertyNames, 
+                                             @Nonnull Iterable<String> propertyNames, 
                                              @Nullable String[] declaringNodeTypeNames, 
                                              @Nonnull String propertyIndexType,
                                              Map<String, String> properties) throws RepositoryException {
 
         NodeBuilder entry = indexNode.child(indexDefName)
-                .setProperty(JCR_PRIMARYTYPE, INDEX_DEFINITIONS_NODE_TYPE, NAME)
-                .setProperty(TYPE_PROPERTY_NAME, propertyIndexType)
-                .setProperty(REINDEX_PROPERTY_NAME, false);
+            .setProperty(JCR_PRIMARYTYPE, INDEX_DEFINITIONS_NODE_TYPE, NAME)
+            .setProperty(TYPE_PROPERTY_NAME, propertyIndexType)
+            .setProperty(REINDEX_PROPERTY_NAME, false);
         if (unique) {
             entry.setProperty(UNIQUE_PROPERTY_NAME, unique);
         }
         entry.setProperty(PropertyStates.createProperty(PROPERTY_NAMES, propertyNames, NAMES));
-        if (declaringNodeTypeNames != null && declaringNodeTypeNames.length>0) {
-            entry.setProperty(PropertyStates.createProperty(DECLARING_NODE_TYPES, declaringNodeTypeNames, NAMES));
+        if (declaringNodeTypeNames != null && declaringNodeTypeNames.length > 0) {
+            entry.setProperty(PropertyStates.createProperty(DECLARING_NODE_TYPES,
+                declaringNodeTypeNames, NAMES));
         }
-        //additional properties
+        // additional properties
         if (properties != null) {
             for (String k : properties.keySet()) {
                 entry.setProperty(k, properties.get(k));
