4ae1551e46bee20c65468281224e975a3165a5dd
Davide Giannella <davide@apache.org>
2014-06-25 16:30:47 +0200

    OAK-1907 Better cost estimates for traversal, property, and ordered indexes
    Initial stub. WIP.

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/util/NodeCounter.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.util;
+
+import javax.annotation.Nonnull;
+
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+
+
+public class NodeCounter {
+    /**
+     * standard prefix for the count property
+     */
+    public static final String PREFIX = ":count-";
+    
+    /**
+     * the standard approximation count resolution
+     */
+    public static final long APPROX_RESOLUTION = 1000L;
+    
+    /**
+     * value returned if during the count no properties has been found
+     */
+    public static final long NO_PROPERTIES = Long.MIN_VALUE;
+    
+    /**
+     * Same as {@link #getApproxAdded(NodeBuilder, String)} providing {@link #PREFIX} as
+     * {@code prefix}
+     * 
+     * @param node
+     * @return
+     */
+    public static final long getApproxAdded(@Nonnull final NodeBuilder node) {
+        return getApproxAdded(node, PREFIX);
+    }
+
+    /**
+     * retrieve the approximate count of the provided node.
+     *
+     * @param node the node to analyse
+     * @param prefix the prefix of the property to inspect
+     * @return
+     */
+    public static final long getApproxAdded(@Nonnull final NodeBuilder node,
+                                            @Nonnull final String prefix) {
+        return NO_PROPERTIES;
+    }
+}
---
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/util/NodeCounterTest.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import org.apache.jackrabbit.oak.plugins.memory.EmptyNodeState;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.junit.Test;
+
+public class NodeCounterTest {
+    public static class MockRandom extends Random {
+        private static final long serialVersionUID = -2051060190069357480L;
+        
+        /**
+         * if passed into the {@link #setNextInt(int)} will instruct the class to use the original
+         * {@code Random.nextInt(int)}
+         */
+        public static final int SUPER = Integer.MIN_VALUE;
+        
+        private int nextInt = SUPER;
+        
+        /**
+         * if previously set to {@link #SUPER} using the {@link #setNextInt(int)} it will work as
+         * {@code Random.nextInt(int)} otherwise it will return the previous value set via
+         * {@link #setNextInt(int)}
+         */
+        @Override
+        public int nextInt(int n) {
+            if (nextInt == SUPER) {
+                return super.nextInt(n);
+            } else {
+                return nextInt;
+            }
+            
+        }
+        
+        public void setNextInt(int nextInt) {
+            this.nextInt = nextInt;
+        }
+    }
+    
+    static final MockRandom RND = new MockRandom();
+    
+    @Test
+    public void getApproxAdded() {
+        NodeBuilder node; 
+        
+        // building the expected structure
+        node = EmptyNodeState.EMPTY_NODE.builder();
+        
+        assertEquals("if the node has no properties for counting NO_PROPERTIES is expected",
+            NodeCounter.NO_PROPERTIES, NodeCounter.getApproxAdded(node));
+    }
+}
