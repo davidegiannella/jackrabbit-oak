580cdb2ccf00fe99ca81d5cff2aee38a135ffa61
Davide Giannella <davide@apache.org>
2014-10-27 15:37:33 +0000

    OAK-2226 - Lucene no results for multiple jcr:contains
    registered the node types

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexAggregationTest2.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/plugins/index/lucene/LuceneIndexAggregationTest2.java
@@ -23,60 +23,123 @@ import static org.apache.jackrabbit.JcrConstants.NT_FILE;
 import static org.apache.jackrabbit.JcrConstants.NT_FOLDER;
 import static org.apache.jackrabbit.oak.plugins.name.NamespaceConstants.REP_NAMESPACES;
 import static org.apache.jackrabbit.oak.plugins.nodetype.NodeTypeConstants.JCR_NODE_TYPES;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.api.ContentRepository;
+import org.apache.jackrabbit.oak.api.Root;
+import org.apache.jackrabbit.oak.core.SystemRoot;
 import org.apache.jackrabbit.oak.plugins.index.aggregate.AggregateIndexProvider;
 import org.apache.jackrabbit.oak.plugins.index.aggregate.NodeAggregator;
 import org.apache.jackrabbit.oak.plugins.index.aggregate.SimpleNodeAggregator;
+import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeStore;
 import org.apache.jackrabbit.oak.plugins.name.NamespaceConstants;
+import org.apache.jackrabbit.oak.plugins.name.NamespaceEditorProvider;
 import org.apache.jackrabbit.oak.plugins.nodetype.NodeTypeConstants;
+import org.apache.jackrabbit.oak.plugins.nodetype.TypeEditorProvider;
 import org.apache.jackrabbit.oak.plugins.nodetype.write.InitialContent;
 import org.apache.jackrabbit.oak.plugins.nodetype.write.NodeTypeRegistry;
 import org.apache.jackrabbit.oak.query.AbstractQueryTest;
+import org.apache.jackrabbit.oak.spi.commit.CompositeEditorProvider;
+import org.apache.jackrabbit.oak.spi.commit.EditorHook;
 import org.apache.jackrabbit.oak.spi.commit.Observer;
 import org.apache.jackrabbit.oak.spi.security.OpenSecurityProvider;
+import org.apache.jackrabbit.oak.spi.state.ApplyDiff;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
 
 public class LuceneIndexAggregationTest2 extends AbstractQueryTest {
-
+    private static final Logger LOG = LoggerFactory.getLogger(LuceneIndexAggregationTest2.class);
+    
     @Override
     protected ContentRepository createRepository() {
         LowCostLuceneIndexProvider provider = new LowCostLuceneIndexProvider();
+        
         return new Oak()
-                .with(new InitialContent() {
+            .with(new InitialContent() {
 
-                    @Override
-                    public void initialize(NodeBuilder builder) {
-                        super.initialize(builder);
-                        
-                        
-                        NodeBuilder namespace = builder.child(JCR_SYSTEM).child(JCR_NODE_TYPES);
-                        for (String node : namespace.getChildNodeNames()) {
-                            System.out.println(node);
+                @Override
+                public void initialize(NodeBuilder builder) {
+                    super.initialize(builder);
+
+                    // registering additional node types for wider testing
+                    InputStream stream = null;
+                    try {
+                        stream = LuceneIndexAggregationTest2.class
+                            .getResourceAsStream("test_nodetypes.cnd");
+                        NodeState base = builder.getNodeState();
+                        NodeStore store = new MemoryNodeStore(base);
+
+                        Root root = new SystemRoot(store, new EditorHook(
+                            new CompositeEditorProvider(new NamespaceEditorProvider(),
+                                new TypeEditorProvider())));
+
+                        NodeTypeRegistry.register(root, stream, "testing node types");
+
+                        NodeState target = store.getRoot();
+                        target.compareAgainstBaseState(base, new ApplyDiff(builder));
+                    } catch (Exception e) {
+                        LOG.error("Error while registering required node types. Failing here", e);
+                        fail("Error while registering required node types");
+                    } finally {
+                        printNodeTypes(builder);
+                        if (stream != null) {
+                            try {
+                                stream.close();
+                            } catch (IOException e) {
+                                LOG.debug("Ignoring exception on stream closing.", e);
+                            }
                         }
-                        
-                        InputStream stream = LuceneIndexAggregationTest2.class.getResourceAsStream("test_nodetypes.cnd");
-                        
-                        NodeTypeRegistry.register(builder, stream, "testing node types");
                     }
-                    
-                })
-                .with(new OpenSecurityProvider())
-                .with(AggregateIndexProvider.wrap(provider.with(getNodeAggregator())))
-                .with((Observer) provider)
-                .with(new LuceneIndexEditorProvider())
-                .createContentRepository();
+
+                }
+
+            })
+            .with(new OpenSecurityProvider())
+            .with(AggregateIndexProvider.wrap(provider.with(getNodeAggregator())))
+            .with((Observer) provider).with(new LuceneIndexEditorProvider())
+            .createContentRepository();
+    }
+    
+    /**
+     * convenience method for printing on logs the currently registered node types.
+     * 
+     * @param builder
+     */
+    private static void printNodeTypes(NodeBuilder builder) {
+        if (LOG.isDebugEnabled()) {
+            NodeBuilder namespace = builder.child(JCR_SYSTEM).child(JCR_NODE_TYPES);
+            List<String> nodes = Lists.newArrayList(namespace.getChildNodeNames());
+            Collections.sort(nodes);
+            for (String node : nodes) {
+                LOG.debug(node);
+            }        
+        }
     }
     
     private static NodeAggregator getNodeAggregator() {
         return new SimpleNodeAggregator()
-            .newRuleWithName(NT_FILE, newArrayList(JCR_CONTENT))
-            .newRuleWithName(NT_FOLDER, newArrayList(JCR_CONTENT));
+            .newRuleWithName("nt:file", newArrayList("jcr:content"))
+            .newRuleWithName("test:Page", newArrayList("jcr:content"))
+            .newRuleWithName("test:PageContent", newArrayList("*", "*/*", "*/*/*", "*/*/*/*"))
+            .newRuleWithName("test:Asset", newArrayList("jcr:content"))
+            .newRuleWithName(
+                "test:AssetContent",
+                newArrayList("metadata", "renditions", "renditions/original", "comments",
+                    "renditions/original/jcr:content"))
+            .newRuleWithName("rep:User", newArrayList("profile"));
     }
     
     @Test
