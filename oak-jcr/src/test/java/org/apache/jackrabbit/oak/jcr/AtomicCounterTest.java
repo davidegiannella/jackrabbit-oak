226f78f82769d22f609051136052909268f5889e
Davide Giannella <davide@apache.org>
2015-01-20 15:06:34 +0000

    OAK-2220: Support for atomic counters
    - initial stub of a dumb test just to check the whole stack

---
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/AtomicCounterTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.jcr;
+
+import java.util.Set;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.apache.jackrabbit.oak.commons.FixturesHelper;
+import org.apache.jackrabbit.oak.commons.FixturesHelper.Fixture;
+import org.apache.jackrabbit.oak.plugins.nodetype.NodeTypeConstants;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AtomicCounterTest extends AbstractRepositoryTest {
+    private static final Set<Fixture> FIXTURES = FixturesHelper.getFixtures();
+    
+    protected AtomicCounterTest(NodeStoreFixture fixture) {
+        super(fixture);
+    }
+
+    @BeforeClass
+    public void assumptions() {
+        // run only on the below fixtures
+        Assume.assumeTrue(FIXTURES.contains(Fixture.SEGMENT_MK));
+    }
+    
+    @Test
+    public void dumb() throws RepositoryException {
+        Session session = getAdminSession();
+        
+        Node root = session.getRootNode();
+        root.addNode("no");
+        root.addNode("yes").addMixin(NodeTypeConstants.MIX_ATOMIC_COUNTER);
+        
+        // TODO complete this stub
+    }
+}
