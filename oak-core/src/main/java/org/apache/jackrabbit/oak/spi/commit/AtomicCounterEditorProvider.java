0c4cc7877191cf5379d44e227693e8dcd5c2d85b
Davide Giannella <davide@apache.org>
2015-01-20 11:52:31 +0000

    OAK-2220: Support for atomic counters
    - initial stub for the editor provider

---
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/spi/commit/AtomicCounterEditorProvider.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jackrabbit.oak.spi.commit;
+
+import org.apache.jackrabbit.oak.api.CommitFailedException;
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
+
+/**
+ * Provides an instance of the correct {@link Editor} if the changes are carried over a node type
+ * with {@link JcrConstants#JCR_MIXINTYPES} containing {@link #MIXIN_ATOMIC_COUNTER}
+ */
+public class AtomicCounterEditorProvider implements EditorProvider {
+    /**
+     * mixin to be have for the AtomicCounterEditor to be returned.
+     */
+    public static final String MIXIN_ATOMIC_COUNTER = "mix:atomicCounter";
+
+    @Override
+    public Editor getRootEditor(NodeState before, NodeState after, NodeBuilder builder,
+                                CommitInfo info) throws CommitFailedException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
