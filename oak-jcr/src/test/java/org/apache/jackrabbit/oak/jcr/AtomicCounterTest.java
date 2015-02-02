f7e5b3c816444721142b551fc1bbc2ddf68fecb6
Davide Giannella <davide@apache.org>
2015-02-02 15:29:34 +0000

    OAK-2220: Support for atomic counters
    - unit test for non-root nodes
    
    # TODO
    
    - javadocs

--- oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/AtomicCounterTest.java
+++ oak-jcr/src/test/java/org/apache/jackrabbit/oak/jcr/AtomicCounterTest.java
@@ -16,45 +16,28 @@
  */
 package org.apache.jackrabbit.oak.jcr;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static org.apache.jackrabbit.oak.plugins.atomic.AtomicCounterEditor.PROP_COUNTER;
 import static org.apache.jackrabbit.oak.plugins.atomic.AtomicCounterEditor.PROP_INCREMENT;
 import static org.apache.jackrabbit.oak.plugins.nodetype.NodeTypeConstants.MIX_ATOMIC_COUNTER;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeTrue;
 
-import java.util.Set;
-
+import javax.annotation.Nonnull;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
-import org.apache.jackrabbit.oak.commons.FixturesHelper;
-import org.apache.jackrabbit.oak.commons.FixturesHelper.Fixture;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class AtomicCounterTest extends AbstractRepositoryTest {
-    /**
-     * set of fixtures provided by the environment
-     */
-    private static final Set<Fixture> FIXTURES = FixturesHelper.getFixtures();
-        
+public class AtomicCounterTest extends AbstractRepositoryTest {        
     public AtomicCounterTest(NodeStoreFixture fixture) {
         super(fixture);
     }
-
-    @BeforeClass
-    public static void assumptions() {
-        // run only on the below fixtures
-        assumeTrue(FIXTURES.contains(Fixture.SEGMENT_MK));
-    }
     
     @Test
-    public void increment() throws RepositoryException {
-        
-        assumeTrue(NodeStoreFixture.SEGMENT_MK.equals(fixture));
+    public void incrementRootNode() throws RepositoryException {
         
         Session session = getAdminSession();
 
@@ -73,39 +56,72 @@ public class AtomicCounterTest extends AbstractRepositoryTest {
             node.addMixin(MIX_ATOMIC_COUNTER);
             session.save();
 
-            assertTrue(node.hasProperty(PROP_COUNTER));
-            assertEquals(0, node.getProperty(PROP_COUNTER).getLong());
-
+            assertCounter(node, 0);
+            
             node.setProperty(PROP_INCREMENT, 1L);
             session.save();
-            assertTrue(node.hasProperty(PROP_COUNTER));
-            assertEquals(1, node.getProperty(PROP_COUNTER).getLong());
-            assertFalse(node.hasProperty(PROP_INCREMENT));
+            assertCounter(node, 1);
 
             // increment again the same node
             node.setProperty(PROP_INCREMENT, 1L);
             session.save();
-            assertTrue(node.hasProperty(PROP_COUNTER));
-            assertEquals(2, node.getProperty(PROP_COUNTER).getLong());
-            assertFalse(node.hasProperty(PROP_INCREMENT));
+            assertCounter(node, 2);
 
             // decrease the counter by 2
             node.setProperty(PROP_INCREMENT, -2L);
             session.save();
-            assertTrue(node.hasProperty(PROP_COUNTER));
-            assertEquals(0, node.getProperty(PROP_COUNTER).getLong());
-            assertFalse(node.hasProperty(PROP_INCREMENT));
+            assertCounter(node, 0);
 
             // increase by 5
             node.setProperty(PROP_INCREMENT, 5L);
             session.save();
-            assertTrue(node.hasProperty(PROP_COUNTER));
-            assertEquals(5, node.getProperty(PROP_COUNTER).getLong());
-            assertFalse(node.hasProperty(PROP_INCREMENT));
+            assertCounter(node, 5);
         } finally {
             session.logout();
         }
     }
     
-    // FIXME add test case on non root node
+    private static void assertCounter(@Nonnull final Node counter, final long expectedCount) 
+                                    throws RepositoryException {
+        checkNotNull(counter);
+        
+        assertTrue(counter.hasProperty(PROP_COUNTER));
+        assertEquals(expectedCount, counter.getProperty(PROP_COUNTER).getLong());
+        assertFalse(counter.hasProperty(PROP_INCREMENT));
+    }
+    
+    @Test
+    public void incrementNonRootNode() throws RepositoryException {
+        Session session = getAdminSession();
+        
+        try {
+            Node counter = session.getRootNode().addNode("foo").addNode("bar").addNode("counter");
+            counter.addMixin(MIX_ATOMIC_COUNTER);
+            session.save();
+            
+            assertCounter(counter, 0);
+            
+            counter.setProperty(PROP_INCREMENT, 1L);
+            session.save();
+            assertCounter(counter, 1);
+
+            // increment again the same node
+            counter.setProperty(PROP_INCREMENT, 1L);
+            session.save();
+            assertCounter(counter, 2);
+
+            // decrease the counter by 2
+            counter.setProperty(PROP_INCREMENT, -2L);
+            session.save();
+            assertCounter(counter, 0);
+
+            // increase by 5
+            counter.setProperty(PROP_INCREMENT, 5L);
+            session.save();
+            assertCounter(counter, 5);
+        } finally {
+            session.logout();
+        }
+    }
+    
 }
