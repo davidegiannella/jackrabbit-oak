18f9d45b57bc6e4f7050a908ae9397cc83b3b37c
Davide Giannella <davide@apache.org>
2015-06-05 10:57:09 +0100

    added logging filter to catch the exception

--- oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/AsyncConflictsIT.java
+++ oak-lucene/src/test/java/org/apache/jackrabbit/oak/jcr/AsyncConflictsIT.java
@@ -21,6 +21,7 @@ import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.ASYNC_PROPE
 import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.REINDEX_PROPERTY_NAME;
 import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.TYPE_PROPERTY_NAME;
 import static org.apache.jackrabbit.oak.plugins.nodetype.NodeTypeConstants.NT_OAK_UNSTRUCTURED;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
 import java.util.Collections;
@@ -30,23 +31,29 @@ import java.util.Random;
 import java.util.Set;
 
 import javax.annotation.Nonnull;
-import javax.jcr.LoginException;
 import javax.jcr.Node;
 import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
-import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.oak.commons.FixturesHelper;
 import org.apache.jackrabbit.oak.commons.FixturesHelper.Fixture;
 import org.apache.jackrabbit.oak.plugins.index.IndexConstants;
 import org.apache.jackrabbit.oak.plugins.index.IndexEditorProvider;
 import org.apache.jackrabbit.oak.plugins.index.lucene.LuceneIndexEditorProvider;
-import org.apache.jackrabbit.oak.plugins.nodetype.NodeTypeConstants;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.read.ListAppender;
+import ch.qos.logback.core.spi.FilterReply;
 
 import com.google.common.collect.ImmutableSet;
 
@@ -66,6 +73,7 @@ public class AsyncConflictsIT extends DocumentClusterIT {
     public void updates() throws Exception {
         final Map<String, Exception> exceptions = Collections.synchronizedMap(new HashMap<String, Exception>());
         final Random generator = new Random(3);
+        final ListAppender<ILoggingEvent> logAppender = subscribeAppender();
         
         setUpCluster(this.getClass(), mks, repos, NOT_PROVIDED);
         defineIndex(repos.get(0));
@@ -112,13 +120,20 @@ public class AsyncConflictsIT extends DocumentClusterIT {
         }
         
         raiseExceptions(exceptions, LOG);
+        
+        // if following fails it means the Async index failed at least once.
+        assertTrue(
+            String.format("We should have not any '%s' in the logs", AsyncLogFilter.MESSAGE),
+            logAppender.list.isEmpty());
+        
+        unsubscribe(logAppender);
     }
     
     private boolean isReindexFinished() throws RepositoryException {
         Session s = repos.get(0).login(ADMIN);
         try {
-            boolean reindex = s.getNode("/oak:index/" + INDEX_DEF_NODE).getProperty(REINDEX_PROPERTY_NAME)
-                .getBoolean();
+            boolean reindex = s.getNode("/oak:index/" + INDEX_DEF_NODE)
+                .getProperty(REINDEX_PROPERTY_NAME).getBoolean();
             return !reindex;
         } finally {
             s.logout();
@@ -159,5 +174,42 @@ public class AsyncConflictsIT extends DocumentClusterIT {
     @Override
     protected boolean isAsyncIndexing() {
         return true;
+    }
+    
+    private ListAppender<ILoggingEvent> subscribeAppender() {
+        Filter<ILoggingEvent> filter = new AsyncLogFilter();
+        filter.start();
+        ListAppender<ILoggingEvent> appender = new ListAppender<ILoggingEvent>();
+        appender.setContext((LoggerContext) LoggerFactory.getILoggerFactory());
+        appender.setName("asynclogcollector");
+        appender.addFilter(filter);
+        appender.start();
+        ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger(
+            ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME).addAppender(appender);
+        return appender;
+        
+    }
+    
+    private void unsubscribe(@Nonnull final Appender<ILoggingEvent> appender) {
+        ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger(
+            ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME).detachAppender(appender);
+    }
+    
+    private static class AsyncLogFilter extends Filter<ILoggingEvent> {
+        public static final String MESSAGE = "Unresolved conflicts in /:async";
+        
+        @Override
+        public FilterReply decide(ILoggingEvent event) {
+            final IThrowableProxy tp = event.getThrowableProxy();
+            
+            if (event.getLevel().isGreaterOrEqual(Level.WARN) &&
+                tp != null && 
+                tp.getMessage().contains(MESSAGE)) {
+                return FilterReply.ACCEPT;
+            } else {
+                return FilterReply.DENY;
+            }
+        }
+        
     }
 }
