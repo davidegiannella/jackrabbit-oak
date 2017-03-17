f74bdef6bea767c2f831bf102c1d68ffd417e6e1
Davide Giannella <davide@apache.org>
2017-03-17 17:13:30 +0000

    Initial Mode class to avoid duplicate code between `run` artifacts

---
+++ oak-commons-run/src/main/java/org/apache/jackrabbit/oak/commons/run/Mode.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.jackrabbit.oak.commons.run;
+
+/**
+ * represent an individual Mode for running a COMMAND. It's a substitution for the old Mode enum we
+ * used in order to allow reuse of logic.
+ */
+public abstract class Mode {
+    public final String NAME;
+    public final Command COMMAND;
+
+    private Mode(){
+        // should prevent this initialisation from extending classes
+        this.NAME = null;
+        this.COMMAND = null;
+    }
+
+    public Mode(String name, Command command) {
+        this.NAME = name;
+        this.COMMAND = command;
+    }
+}
